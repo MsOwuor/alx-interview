4b26b1fdc60237e779a665ee6d92cb22353de2c0
MsOwuor <immaculateadhiambo79@gmail.com>
2024-05-03 08:40:50 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,40 @@
 #!/usr/bin/python3
 """
-Log parsing script
+Log parsing
 """
 
 import sys
-import re
 
+if __name__ == '__main__':
 
-def output(log: dict) -> None:
-    """
-    Helper function to display stats
-    :param log: Dictionary containing the log statistics
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-
-if __name__ == "__main__":
-    # Regular expression to match the log line format
-    regex = re.compile(
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
-        r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] '
-        r'"GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if match:
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
 
-                # Update file size
-                log["file_size"] += file_size
-
-                # Update status code frequency
-                if code.isdecimal():
-                    log["code_frequency"][code] += 1
-
-                # Print stats every 10 lines
-                if line_count % 10 == 0:
-                    output(log)
-    finally:
-        # Print final stats
-        output(log)
